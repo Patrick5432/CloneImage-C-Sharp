49147e2510ecc5fc24c613bb9959109634b2fbdf
fawefaw <maxx5326@gmail.com>
2023-11-08 09:31:58 +0500

    Переделывание картинки в байты и клонирование картинки при помощи байтов

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string imagePath = "./testimg/cat-animals-pet-kitten.jpeg";
+        byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+        Console.WriteLine("Размер массива байтов изображения: " + imageBytes.Length);
+        string path = "./cloneImg/clone.jpeg";
+        File.WriteAllBytes(path, imageBytes);
+        Console.WriteLine("Файл успешно скопирован");
+    }
+}
